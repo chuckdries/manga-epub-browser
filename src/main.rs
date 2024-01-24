358885019d27739f87da7938644f35e0add64c37
Chuck Dries <chuck@chuckdries.com>
2024-01-24 02:14:34 -0800

    add configuration support and begin querying chapters

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use anyhow::{anyhow, Error, Result};
 use askama::Template;
@@ -6,15 +6,43 @@ use axum::{
     debug_handler,
     extract::{Path, Query},
     http::StatusCode,
-    response::{IntoResponse, Response, Html},
+    response::{Html, IntoResponse, Response},
     routing::get,
-    Router,
+    Extension, Router,
 };
 use graphql_client::{reqwest::post_graphql, GraphQLQuery};
 use manga_search_by_title::MangaSearchByTitleMangasNodes;
 use reqwest;
-// use serde::Serialize;
 
+use config::{builder::DefaultState, ConfigBuilder, ConfigError, Environment, File};
+use serde::Deserialize;
+use tokio::sync::RwLock;
+use util::join_url;
+
+mod util; // Declare the util module
+
+#[derive(Deserialize)]
+struct AppConfig {
+    suwayomi_url: String,
+    // Add other configuration fields here
+}
+
+impl AppConfig {
+    pub fn new() -> Result<Self, ConfigError> {
+        let builder = ConfigBuilder::<DefaultState>::default()
+            .add_source(File::with_name("config/default.toml").required(false))
+            .add_source(Environment::with_prefix("APP").separator("__"))
+            .build()?;
+
+        builder.try_deserialize::<AppConfig>()
+    }
+}
+
+struct AppState {
+    config: RwLock<AppConfig>, // Use RwLock for thread-safe access
+}
+
+// CQ clean up this error handling mess
 // Make our own error that wraps `anyhow::Error`.
 struct AppError(anyhow::Error);
 
@@ -66,12 +94,13 @@ pub struct MangaSearchByTitle;
 
 async fn search_manga_by_title(
     variables: manga_search_by_title::Variables,
+    base_url: &str,
 ) -> Result<Vec<manga_search_by_title::MangaSearchByTitleMangasNodes>, Error> {
     let client = reqwest::Client::new();
 
     return match post_graphql::<MangaSearchByTitle, _>(
         &client,
-        "http://10.10.11.250:4567/api/graphql",
+        join_url(base_url, "/api/graphql")?,
         variables,
     )
     .await?
@@ -80,7 +109,7 @@ async fn search_manga_by_title(
         Some(data) => {
             println!("{:#?}", data.mangas.nodes);
             Ok(data.mangas.nodes)
-        },
+        }
         None => Err(anyhow!("Missing response data")),
     };
 }
@@ -90,22 +119,27 @@ async fn search_manga_by_title(
 struct SearchResultsTemplate {
     title: String,
     mangas: Vec<MangaSearchByTitleMangasNodes>,
-    api_base: &'static str,
+    api_base: String,
 }
 
 #[debug_handler]
 async fn search_results(
+    Extension(shared_state): Extension<Arc<AppState>>,
     Query(params): Query<HashMap<String, String>>,
 ) -> Result<SearchResultsTemplate, AppError> {
+    let api_base = &shared_state.config.read().await.suwayomi_url;
     let title = params.get("title").unwrap().to_string();
-    let res = search_manga_by_title(manga_search_by_title::Variables {
-        title: title.clone(),
-    })
+    let res = search_manga_by_title(
+        manga_search_by_title::Variables {
+            title: title.clone(),
+        },
+        api_base,
+    )
     .await?;
     Ok(SearchResultsTemplate {
         title: title,
         mangas: res,
-        api_base: "http://10.10.11.250:4567"
+        api_base: api_base.clone(),
     })
 }
 
@@ -119,12 +153,13 @@ pub struct SpecificMangaById;
 
 async fn get_manga_by_id(
     id: i64,
+    base_url: &str,
 ) -> Result<specific_manga_by_id::SpecificMangaByIdManga, Error> {
     let client = reqwest::Client::new();
 
     return match post_graphql::<SpecificMangaById, _>(
         &client,
-        "http://10.10.11.250:4567/api/graphql",
+        join_url(base_url, "/api/graphql")?,
         specific_manga_by_id::Variables { id },
     )
     .await?
@@ -139,16 +174,29 @@ async fn get_manga_by_id(
 #[template(path = "manga-page.html")]
 struct MangaPageTemplate {
     manga: specific_manga_by_id::SpecificMangaByIdManga,
-    api_base: &'static str,
+    api_base: String,
 }
 
 #[debug_handler]
 async fn manga_by_id(
-    params: Path<i64>
+    Extension(shared_state): Extension<Arc<AppState>>,
+    params: Path<i64>,
 ) -> Result<MangaPageTemplate, AppError> {
-    let manga = get_manga_by_id(params.0).await?;
-    Ok(MangaPageTemplate{ manga, api_base: "http://10.10.11.250:4567" })
+    let api_base = &shared_state.config.read().await.suwayomi_url;
+    let manga = get_manga_by_id(params.0, api_base).await?;
+    Ok(MangaPageTemplate {
+        manga,
+        api_base: api_base.clone(),
+    })
 }
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.json",
+    query_path = "graphql/queries/SpecificMangaChapters.graphql",
+    response_derives = "Debug"
+)]
+pub struct SpecificMangaChapters;
 
 #[derive(Template)]
 #[template(path = "home.html")]
@@ -161,12 +209,17 @@ async fn home() -> Result<HomeTemplate, AppError> {
 
 #[tokio::main]
 async fn main() {
+    let config = AppConfig::new().expect("Failed to load configuration");
+    let state = Arc::new(AppState {
+        config: RwLock::new(config),
+    });
     // build our application with a single route
     let app = Router::new()
         .route("/", get(home))
         .route("/search", get(search_results))
         .route("/manga/:id", get(manga_by_id))
-        .fallback(not_found);
+        .fallback(not_found)
+        .layer(Extension(state.clone()));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
---
+++ src/util.rs
@@ -0,0 +1,7 @@
+use url::Url;
+
+pub fn join_url(base_url: &str, path: &str) -> Result<Url, url::ParseError> {
+  let parsed_base_url = Url::parse(base_url)?;
+  let joined_url = parsed_base_url.join(path)?;
+  Ok(joined_url)
+}
